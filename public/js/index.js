0411b9e7b2ba3ba6845a80e9ce12c54b4d30f7df
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2024-06-24 00:40:58 -0500

    fix autoplay video

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,20 @@
 const menu = $("#menu");
 
 const defaultMenu = () => {
-    if($(window).width() >= 1024)
-        menu.removeAttr("style");
-}
+  if ($(window).width() >= 1024) menu.removeAttr("style");
+};
 
 const menuToggle = () => {
-    menu.toggle("blind", {}, 500);
-}
+  menu.toggle("blind", {}, 500);
+};
 
 $(window).on("resize", defaultMenu);
 $("#menu-toggle-open-btn").on("click", menuToggle);
 $("#menu-toggle-close-btn").on("click", menuToggle);
+// $(document).ready(() => {
+//   const video = document.getElementById("video");
+//   video.play().catch(function (error) {
+//     console.error("Autoplay prevented:", error);
+//     // Handle autoplay prevention
+//   });
+// });
