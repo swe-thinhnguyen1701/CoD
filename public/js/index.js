7d890adb5dfa199947d6754a935f2f8c5d56536c
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2024-06-24 00:16:42 -0500

    add video section, fix alignment

--- public/js/index.js
+++ public/js/index.js
@@ -1,6 +1,14 @@
-const menuToggle = () => {
-    $("#menu").toggle("blind", {}, 500);
+const menu = $("#menu");
+
+const defaultMenu = () => {
+    if($(window).width() >= 1024)
+        menu.removeAttr("style");
 }
 
+const menuToggle = () => {
+    menu.toggle("blind", {}, 500);
+}
+
+$(window).on("resize", defaultMenu);
 $("#menu-toggle-open-btn").on("click", menuToggle);
 $("#menu-toggle-close-btn").on("click", menuToggle);
