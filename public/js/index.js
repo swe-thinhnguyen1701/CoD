76bb80a5d83f2880a88b9769fbf5ce83274fc584
Thinh Nguyen <swe.thinhnguyen@gmail.com>
2024-06-27 20:34:15 -0500

    add calculation method to compute total universal speed up.

--- public/js/index.js
+++ public/js/index.js
@@ -1,4 +1,5 @@
 const menu = $("#menu");
+const amountInput = $(".us-amount-input");
 
 const defaultMenu = () => {
   if ($(window).width() >= 1024) menu.removeAttr("style");
@@ -8,17 +9,35 @@ const menuToggle = () => {
   menu.toggle("blind", {}, 500);
 };
 
-const preventPhoneScreen = () => {
-  
+const usCalculator = () => {
+  let total = 0;
+  amountInput.each(function () {
+    if ($(this).val().trim().length != 0)
+      total += parseInt($(this).val().trim()) * parseInt($(this).data("val"));
+  });
+  const dd = total / 1440;
+  const hh = (dd % 1) * 24;
+  const mm = (hh % 1) * 60;
+  const displayTotal = $("#us-total");
+  displayTotal.empty();
+  $("#us-total").append(`<span class="fs-20">${Math.floor(dd)}</span>D <span class="fs-20">${Math.floor(hh)}</span>H <span class="fs-20">${mm}</span>M`);
 }
+
+amountInput.on("keydown", e => {
+  console.log(e.key);
+  const key = e.key;
+  const isNumber = /^[0-9]$/;
+  const isNavigationKey = ['Backspace', 'ArrowLeft', 'ArrowRight', 'Delete', 'Tab'].includes(key);
+
+  if (!isNumber.test(key) && !isNavigationKey) {
+    e.preventDefault();
+    return;
+  };
+
+
+  usCalculator();
+});
 
 $(window).on("resize", defaultMenu);
 $("#menu-toggle-open-btn").on("click", menuToggle);
 $("#menu-toggle-close-btn").on("click", menuToggle);
-// $(document).ready(() => {
-//   const video = document.getElementById("video");
-//   video.play().catch(function (error) {
-//     console.error("Autoplay prevented:", error);
-//     // Handle autoplay prevention
-//   });
-// });
